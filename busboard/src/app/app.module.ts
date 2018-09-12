e92446ed86235c1954342ecfb79e4fbcfc84c442
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2018-09-12 11:44:04 +0100

    User enters their own choice for postcode etc

--- busboard/src/app/app.module.ts
+++ busboard/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StopsComponent } from './components/stops/stops.component';
@@ -16,7 +17,8 @@ import { StopDetailComponent } from './components/stop-detail/stop-detail.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- busboard/src/app/components/stops/stops.component.ts
+++ busboard/src/app/components/stops/stops.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { Stop } from '../../models/stop';
 import { StopsService } from '../../services/stops.service';
@@ -8,18 +11,35 @@ import { StopsService } from '../../services/stops.service';
   templateUrl: './stops.component.html',
   styleUrls: ['./stops.component.scss']
 })
-export class StopsComponent implements OnInit {
+export class StopsComponent implements OnInit, OnDestroy {
 
-  private postcode: string = 'se20ul';
-  private radius: number = 1000;
-  private maxStops: number = 5;
+  postcode: string = 'se20ul';
+  radius: number = 1000;
+  maxStops: number = 5;
 
   stops: Stop[];
+  private stopsSubscription: Subscription = new Subscription();
+
+  @ViewChild('form') form;
 
   constructor(private stopsService: StopsService) {
   }
 
   ngOnInit() {
-    this.stopsService.getStops(this.postcode, this.radius, this.maxStops).subscribe(stops => this.stops = stops);
+    this.form.valueChanges
+      .pipe(debounceTime(500))
+      .pipe(distinctUntilChanged())
+      .subscribe(() => this.refreshStops());
+    this.refreshStops();
+  }
+
+  ngOnDestroy() {
+    this.stopsSubscription.unsubscribe();
+  }
+
+  refreshStops() {
+    this.stops = [];
+    this.stopsSubscription.unsubscribe();
+    this.stopsSubscription = this.stopsService.getStops(this.postcode, this.radius, this.maxStops).subscribe(stops => this.stops = stops);
   }
 }
